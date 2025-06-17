0f554b4c2587da9f65e8fbc06b32dfb907bb5e1e
lucas_baldessar <144919821+lucasbaldessar@users.noreply.github.com>
2025-06-17 07:45:45 -0300

    .

--- js main.js
+++ js main.js
@@ -1,6 +1,17 @@
-Const botoes = document.querySelectorALL(".botao");
-Const botoes = document.querySelectorALL(".aba-conteudo");
-for (let i=0; i<botoes.length; i++){
-     botoes[j].classlit
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
 }
